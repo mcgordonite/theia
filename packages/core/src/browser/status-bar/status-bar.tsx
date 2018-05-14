00803d3d6ef1bfa12cff594dc8edec4786452da4
Jan Bicker <jan.bicker@typefox.io>
2018-05-14 09:25:45 +0200

    react migration
    Signed-off-by: Jan Bicker <jan.bicker@typefox.io>

--- packages/core/src/browser/status-bar/status-bar.tsx
+++ packages/core/src/browser/status-bar/status-bar.tsx
@@ -1,16 +1,26 @@
 /*
- * Copyright (C) 2017 TypeFox and others.
+ * Copyright (C) 2017-2018 TypeFox and others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-import { VirtualRenderer, VirtualWidget } from '../widgets';
 import { CommandService } from '../../common';
-import { h, ElementInlineStyle } from '@phosphor/virtualdom';
 import { LabelParser, LabelIcon } from '../label-parser';
 import { injectable, inject } from 'inversify';
 import { FrontendApplicationStateService } from '../frontend-application-state';
+import { ReactWidget } from '../widgets/react-widget';
+import * as React from "react";
+
+export interface StatusBarLayoutData {
+    entries: StatusBarEntryData[]
+    backgroundColor?: string
+}
+
+export interface StatusBarEntryData {
+    id: string;
+    entry: StatusBarEntry
+}
 
 export interface StatusBarEntry {
     /**
@@ -41,10 +51,12 @@ export enum StatusBarAlignment {
 }
 
 export interface StatusBarEntryAttributes {
-    style?: ElementInlineStyle;
     className?: string;
     title?: string;
-    onclick?: (e: MouseEvent) => void;
+    style?: object;
+    onClick?: (e: MouseEvent) => void;
+    onMouseOver?: () => void;
+    onMouseOut?: () => void;
 }
 
 export const STATUSBAR_WIDGET_FACTORY_ID = 'statusBar';
@@ -58,7 +70,7 @@ export interface StatusBar {
 }
 
 @injectable()
-export class StatusBarImpl extends VirtualWidget implements StatusBar {
+export class StatusBarImpl extends ReactWidget implements StatusBar {
 
     protected backgroundColor: string | undefined;
     protected entries: Map<string, StatusBarEntry> = new Map();
@@ -100,9 +112,27 @@ export class StatusBarImpl extends VirtualWidget implements StatusBar {
         this.node.style.backgroundColor = this.backgroundColor ? this.backgroundColor : null;
     }
 
-    protected render(): h.Child {
-        const leftEntries: h.Child[] = [];
-        const rightEntries: h.Child[] = [];
+    getLayoutData(): StatusBarLayoutData {
+        const entries: StatusBarEntryData[] = [];
+        this.entries.forEach((entry, id) => {
+            entries.push({ id, entry });
+        });
+        return { entries, backgroundColor: this.backgroundColor };
+    }
+
+    setLayoutData(data: StatusBarLayoutData): void {
+        if (data.entries) {
+            data.entries.forEach(entryData => {
+                this.entries.set(entryData.id, entryData.entry);
+            });
+            this.update();
+        }
+        this.internalSetBackgroundColor(data.backgroundColor);
+    }
+
+    protected render(): JSX.Element[] {
+        const leftEntries: JSX.Element[] = [];
+        const rightEntries: JSX.Element[] = [];
         const elements = Array.from(this.entries.values()).sort((left, right) => {
             const lp = left.priority || 0;
             const rp = right.priority || 0;
@@ -115,24 +145,29 @@ export class StatusBarImpl extends VirtualWidget implements StatusBar {
                 rightEntries.push(this.renderElement(entry));
             }
         });
-        const leftElements = h.div({ className: 'area left' }, VirtualRenderer.flatten(leftEntries));
-        const rightElements = h.div({ className: 'area right' }, VirtualRenderer.flatten(rightEntries));
-        return VirtualRenderer.flatten([leftElements, rightElements]);
+        const leftElements = <div className="area left"><React.Fragment>{leftEntries}</React.Fragment></div>;
+        const rightElements = <div className="area right"><React.Fragment>{rightEntries}</React.Fragment></div>;
+
+        return [leftElements, rightElements];
+    }
+
+    protected onclick(entry: StatusBarEntry): () => void {
+        return () => {
+            if (entry.command) {
+                const args = entry.arguments || [];
+                this.commands.executeCommand(entry.command, ...args);
+            }
+        };
     }
 
     protected createAttributes(entry: StatusBarEntry): StatusBarEntryAttributes {
         const attrs: StatusBarEntryAttributes = {};
 
         if (entry.command) {
-            attrs.onclick = () => {
-                if (entry.command) {
-                    const args = entry.arguments || [];
-                    this.commands.executeCommand(entry.command, ...args);
-                }
-            };
+            attrs.onClick = this.onclick(entry);
             attrs.className = 'element hasCommand';
         } else if (entry.onclick) {
-            attrs.onclick = (e) => {
+            attrs.onClick = e => {
                 if (entry.onclick) {
                     entry.onclick(e);
                 }
@@ -159,21 +194,22 @@ export class StatusBarImpl extends VirtualWidget implements StatusBar {
         return attrs;
     }
 
-    protected renderElement(entry: StatusBarEntry): h.Child {
+    protected renderElement(entry: StatusBarEntry): JSX.Element {
         const childStrings = this.entryService.parse(entry.text);
-        const children: h.Child[] = [];
+        const children: JSX.Element[] = [];
 
         childStrings.forEach((val, idx) => {
+            const key = entry.alignment + "-" + idx;
             if (!(typeof val === 'string') && LabelIcon.is(val)) {
                 const classStr = `fa fa-${val.name} ${val.animation ? 'fa-' + val.animation : ''}`;
-                children.push(h.span({ className: classStr }));
+                children.push(<span className={classStr} key={key}></span>);
             } else {
-                children.push(h.span({}, val));
+                children.push(<span key={key}>{val}</span>);
             }
         });
-        const elementInnerDiv = h.div(VirtualRenderer.flatten(children));
+        const elementInnerDiv = <div>{children}</div>;
 
-        return h.div(this.createAttributes(entry), elementInnerDiv);
+        return React.createElement("div", this.createAttributes(entry), elementInnerDiv);
     }
 
 }
